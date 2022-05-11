488ea41412b0afafb8663c6970fc6ae4fce6f4df
a1601999 <424656153@163.com>
2022-05-11 22:17:26 +0930

    Finished the swapV function

--- quickSort/quickSort.asm
+++ quickSort/quickSort.asm
@@ -10,232 +10,65 @@ M=D
 D=M
 @sz0
 M=D
-@L
-@G
-@P
 
-// since we are going to do the quick sorting recursively, need to use stack
-// setup Stack bootom position
-@256 // Stack bottom at 256, grows towards higher address
-D=A
-@SP
-M=D
-@ARG
-M=D
-@LCL
-M=D
-
-
-(main)
-// prepare funciton calling argument
-@arr0
-D=M
-@SP
-A=M
-M=D // put the pointer to the array to the ARG (now same as SP)
-@SP
-M=M+1 // incrment SP
-// now ARG[0] is arr[0]
-
+// main() {
+//     int16_t* end = arr + sz - 1;
+//     quickSort(arr, end);
+//......
+// }
 @arr0
 D=M
 @sz0
 D=D+M
-D=D-1 // the end of array is at arr0 + sz0 -1 as array starts from arr[0]
-@SP
-A=M
+D=D-1
+@end0
 M=D
-@SP
-M=M+1 // incrment SP
-// now ARG[1] is arr[end-1]
 
-@main.return
-D=A
-@SP
-A=M
-M=D
-@SP
-M=M+1 // incrment SP
+// get stack ready and call quickSort funcion.
 
-@ARG
-D=M
-@SP
-A=M
-M=D // save the caller ARG to stack
-@SP
-M=M+1 // incrment SP
 
-// call quckSort function
-@quickSort
-0;JMP
-
-(main.return)
-@R0
-M=-1 // put -1 in R0 to indicate finish
-(END) // return the main function, halt.
+(END)
 @END
 0;JMP
 
-(quickSort)
-// load subend from from argument
-@ARG
-A=M
-A=A+1 // subend at ARG[1]
-D=M 
-@ARG // subaddr at ARG[0]
-A=M
-D=D-M // subend - subarr
-@quickSort.subarr_sz_GT_1 // then do quckSort on this sub string
-D=D-1;JGT
-@quickSort.return // resursion control condition return
-0;JMP // return if the subarr only has one number (or none)
+// For small functions
+// let R13 = a, R14 = b, R15 = return value
+// let R10 to be return address
 
-(quickSort.subarr_sz_GT_1)
-@ARG
-A=M
-D=M // load pointer to the subarr
-@L
-M=D // put it into L pointer
-@ARG
-A=M
-A=A+1
-D=M // load pointer to the subend
-@G
-M=D // put it into G pointer
 
-// seek for middle point
-(quickSort.While_L_LT_G)
-@L
+// void swapV(int16_t* a, int16_t* b)
+(swapV)
+@R13
+A=M
 D=M
-@G
-D=D-M
-@quickSort.While_L_LT_G_EXIT
-D;JGE
+@temp
+M=D
 
-@L
-M=M+1 // L++
-@G
-M=M-1 // G--
+@R14
+A=M
+D=M
+@R13
+A=M
+M=D
 
-@quickSort.While_L_LT_G
+@temp
+D=M
+@R14
+A=M
+M=D
+
+(swapV$return)
+@R10
+A=M
 0;JMP
 
-(quickSort.While_L_LT_G_EXIT)
-@G
+
+// For small functions
+// let R13 = a, R14 = b, R15 = return value
+// let R10 to be return address
+
+// bool LTA(int16_t* a, int16_t* b)
+@R13
 D=M
-@P
-M=D //P = G;  // select the middle element as the pivoit
-
-// reset Lesser Greater point to the two subend of the array
-@ARG
-A=M
-D=M // load pointer to the subarr
-@L
-M=D // put it into L pointer
-@ARG
-A=M
-A=A+1
-D=M // load pointer to the subend
-@G
-M=D // put it into G pointer
-
-// swap P and G to put the pivoit out of the way, to the end
-// call swap function
-@ARG
-D=M
-@SP
-A=M
-M=D // save the ARG of quickSrot to the stack before calling swap
-@SP
-M=M+1 // incrment SP
-
-// Problem why there no argument for the first layer quickSort?
-/////////////////////////////////////////////////////////////////
-
-@SP
-D=M
-@ARG
-M=D // move ARG up, now ARG is swap's
-@SP
-M=M+1 // incrment SP
-
-@P
-D=M
-@ARG
-A=M
-M=D
-
-@G
-D=M
-@ARG
-A=M
-A=A+1
-M=D
-
-// setup return address
-@quickSort.P_G_swap_exit
-D=A
-@ARG
-A=M
-A=A+1
-A=A+1
-M=D
-
-@swap
-0;JMP // call the swap function
-
-(quickSort.P_G_swap_exit)
-@SP
-A=M-1 
-D=M
-@ARG
-M=D // recover quickSort's ARG
-@SP
-M=M-1 // decrment SP
-
-(quickSort.return)
-@ARG
-A=M
-A=A+1
-A=A+1
-0;JMP // jump to the return address at ARG[2]
-
-(swap)
-// get a and b from the stack
-@ARG
-D=M
-@a
-M=D
-@ARG
-D=M+1
-@b
-M=D
-
-// temp = *a;
-@a
-A=M
-D=M // dereference a to get the value
-@temp
-M=D
-
-// *a = *b;
-@b
-A=M
-D=M
-@a
-A=M
-M=D
-
-// *b = temp;
-@temp
-D=M
-@b
-A=M
-M=D
-
-(swap.return)
-// load the return address
-@ARG
-A=A+1
-A=A+1
-0;JMP // return
+@R14
+D=D-M
